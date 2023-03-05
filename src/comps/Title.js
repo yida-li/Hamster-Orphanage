8c6995d3dc92c7686b026a4e7ecd870846b06af2
yida-li <gabrielyidali@example.com>
2023-03-05 12:28:34 -0800

    basic boiler plate template ready

---
+++ src/comps/Title.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <br>
+      </br>
+      <h2>
+        Hamsters Adopted so far
+      </h2>
+      <p>Upload your new hamster</p>
+    </div>
+  )
+}
+
+export default Title;
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,13 @@ import 'firebase/storage';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgAv9BxFs1WCxBClK0YJpZ8bQokdWr8bw",
-    authDomain: "hamster-cc378.firebaseapp.com",
-    projectId: "hamster-cc378",
-    storageBucket: "hamster-cc378.appspot.com",
-    messagingSenderId: "294289844954",
-    appId: "1:294289844954:web:a3e560f0b9db24788f93e3"
-  };
-
+  apiKey: "AIzaSyAgAv9BxFs1WCxBClK0YJpZ8bQokdWr8bw",
+  authDomain: "hamster-cc378.firebaseapp.com",
+  projectId: "hamster-cc378",
+  storageBucket: "hamster-cc378.appspot.com",
+  messagingSenderId: "294289844954",
+  appId: "1:294289844954:web:a3e560f0b9db24788f93e3"
+};
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
